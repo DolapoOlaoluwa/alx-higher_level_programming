d36faea32920863a5133455b2d40e45e6e790541
Dolapo Olaoluwa <dollipissulelectrical16@gmail.com>
2024-01-21 23:09:45 +0100

    correction on task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
+# Author - Olaoluwa Dolapo
 
 """Print numbers 0 to 98 in decimal and hexadecimal."""
 for number in range(0, 99):
-    print("{} = {}".format(number, hex(number)))
+    print(f"{number} = {hex(number)}")
